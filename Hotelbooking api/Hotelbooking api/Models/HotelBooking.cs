4fc457e20dcd8dfcaf3281f0d6af5c81cdd757c6
hilman45 <163102488+hilman45@users.noreply.github.com>
2024-03-13 09:26:59 +0800

    Add files via upload

---
+++ Hotelbooking api/Hotelbooking api/Models/HotelBooking.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelBookingAPI.Models
+{
+    public class HotelBooking
+    {
+        public int Id { get; set; }
+        public int Roomnumber { get; set; }
+        public string? Client { get; set; }
+    }
+}
